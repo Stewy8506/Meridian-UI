cd35c51eba88fff90bed8a5f19f068d3861e4a72
Stewy8506 <dasanuvab38@gmail.com>
2026-05-29 01:27:10 +0530

    .

--- backend/app/database/models/conversation.py
+++ backend/app/database/models/conversation.py
@@ -24,5 +24,3 @@ class Message(Base):
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
     
     conversation = relationship("Conversation", back_populates="messages")
-
-print("hello world")
